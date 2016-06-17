e480b4f525edf7c612c4598a8944ab1fdf5e7d29
Spoorthi <spoorthi.pujari@gmail.com>
2016-06-17 18:04:55 +0530

    2 apis combined

--- APIcall/app/src/main/java/com/example/quikr/apicall/MainActivity.java
+++ APIcall/app/src/main/java/com/example/quikr/apicall/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.quikr.apicall;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +19,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     TextView responseView;
     ProgressBar progressBar;
     //static final String API_KEY = "USE_YOUR_OWN_API_KEY";
-    static final String API_URL = "http://192.168.124.53:9000/realestate/v1/getShortlistEntities?userId=5";
+    static final String API_URL = "http://192.168.124.53:9000/realestate/v1/getShortlistEntities?userId=20622685";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+    class RetrieveFeedTask extends AsyncTask<Void, Void, List<String>> {
 
         private Exception exception;
 
@@ -56,10 +58,9 @@ public class MainActivity extends AppCompatActivity {
             responseView.setText("");
         }
 
-        protected String doInBackground(Void... urls) {
+        protected List<String> doInBackground(Void... urls) {
             //String email = emailText.getText().toString();
             // Do some validation here
-
             try {
                 URL url = new URL(API_URL);
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
@@ -69,45 +70,71 @@ public class MainActivity extends AppCompatActivity {
                 urlConnection.setRequestProperty("X-Quikr-Signature-v2", "573b66d3cafed4b0682152f3802b166e070f4398");
                 urlConnection.setRequestProperty("Content-Type", "application/json");
 
-                try {
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        stringBuilder.append(line).append("\n");
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                String s = stringBuilder.toString();
+                urlConnection.disconnect();
+
+
+                JSONObject object = (JSONObject) new JSONTokener(s).nextValue();
+                JSONArray requestIDs = object.getJSONArray("data");
+                List<String> aggregate = new ArrayList<>();
+                for (int i = 0; i < requestIDs.length(); i++) {
+                    if (requestIDs.getJSONObject(i).getString("entityName").equals("LISTING")) {
+
+                        try {
+                            url = new URL("http://192.168.124.53:7000/adById?id=" + requestIDs.getJSONObject(i).getLong("entityId"));
+                            urlConnection = (HttpURLConnection) url.openConnection();
+                            urlConnection.setRequestProperty("X-Quikr-App-Id", "752");
+                            urlConnection.setRequestProperty("X-Quikr-Client", "realestate");
+                            urlConnection.setRequestProperty("X-Quikr-Token-Id", "72364402");
+                            urlConnection.setRequestProperty("X-Quikr-Signature-v2", "573b66d3cafed4b0682152f3802b166e070f4398");
+                            urlConnection.setRequestProperty("Content-Type", "application/json");
+
+                            bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                            stringBuilder = new StringBuilder();
+                            while ((line = bufferedReader.readLine()) != null) {
+                                stringBuilder.append(line).append("\n");
+                            }
+                            bufferedReader.close();
+                            System.out.println("siddu  "+stringBuilder.toString());
+                            urlConnection.disconnect();
+                            aggregate.add(stringBuilder.toString());
+
+                        } catch (Exception e) {
+                            Log.e("ERROR", e.getMessage(), e);
+                            return null;
+                        }
                     }
-                    bufferedReader.close();
-                    return stringBuilder.toString();
                 }
-                finally{
-                    urlConnection.disconnect();
-                }
-            }
-            catch(Exception e) {
+                return aggregate;
+            } catch (Exception e) {
                 Log.e("ERROR", e.getMessage(), e);
                 return null;
             }
         }
 
-        protected void onPostExecute(String response) {
-            if(response == null) {
-                response = "THERE WAS AN ERROR";
-            }
-            progressBar.setVisibility(View.GONE);
-            Log.i("INFO", response);
-            //responseView.setText(response);
-            // TODO: check this.exception
-            // TODO: do something with the feed
+            protected void onPostExecute (List<String> response){
+                progressBar.setVisibility(View.GONE);
+                Log.i("INFO", response.get(0));
+                //responseView.setText(response);
+                // TODO: check this.exception
+                // TODO: do something with the feed
 
-            try {
-                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
-                JSONArray requestID = object.getJSONArray("data");
-                responseView.setText(requestID.toString());
+                try {
+                    JSONObject object = (JSONObject) new JSONTokener(response.get(0)).nextValue();
+                    //JSONArray requestID = object.getJSONArray("data");
+                    responseView.setText(object.toString());
 //                int likelihood = object.getInt("likelihood");
 //                JSONArray photos = object.getJSONArray("photos");
-            } catch (JSONException e) {
-                e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
             }
         }
-    }
-}
